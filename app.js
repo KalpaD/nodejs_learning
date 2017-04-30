96c9626676dcfe7c0e9bd224092f873747a5fcea
Kalpa Senanayake <senanayake.kalpa@gmail.com>
2017-04-30 12:27:33 +1000

    Demo object literals

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 var greet = require('./greet');
 greet();
+
+
+var person = {
+    firstName : 'Jhone',
+    lastName : 'Doe',
+    greet : function() {
+        console.log('Hello ' + this.firstName +' '+ this.lastName);
+    }
+};
+
+person.greet();
