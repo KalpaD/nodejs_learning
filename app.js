55ce881468a81b8e96254216106d3b06938b5d5b
Kalpa Senanayake <senanayake.kalpa@gmail.com>
2017-04-30 10:20:33 +1000

    Demonestrating the first class support funtions in javascript and function expressions

---
+++ app.js
@@ -0,0 +1,18 @@
+
+function greet() {
+    console.log('Hello I am node talking to you...');
+}
+
+// functions are first-class citizans in javascript
+function executor(func) {
+    func();
+}
+
+executor(greet);
+
+
+var expression = function() {
+    console.log('Hello this is from the expression..');
+}
+
+executor(expression);
