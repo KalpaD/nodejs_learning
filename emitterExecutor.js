7db49e016e45ee3d85360c422595e16c5c543201
Kalpa Senanayake <senanayake.kalpa@gmail.com>
2017-05-02 21:19:12 +1000

    Improve emitter executor to use node code events and event config

--- emitterExecutor.js
+++ emitterExecutor.js
@@ -1,17 +1,19 @@
-var Emitter = require('./emitter');
-
+// using the node code emitter so called 'events'
+var Emitter = require('events');
+// get the event configurations event object
+var eventConfig = require('./config').eventConfig.events;
 var emitter = new Emitter(); 
 
-//adding functions to emit type 'greet'
-emitter.on('greet', function() {
+// adding functions to emit type 'greet'
+emitter.on(eventConfig.GREET, function() {
     console.log('First emiter function syas hello!');
 });
 
-//adding functions to emit type 'greet'
-emitter.on('greet', function() {
+// adding functions to emit type 'greet'
+emitter.on(eventConfig.GREET, function() {
     console.log('Second emiter function syas hola!');
 });
 
 console.log('Hello');
 // emitting the type 'greet'
-emitter.emit('greet');
+emitter.emit(eventConfig.GREET);
