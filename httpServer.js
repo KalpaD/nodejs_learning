a1967dbc45d32fa764f1553949f4eaa511a78155
Kalpa Senanayake <senanayake.kalpa@gmail.com>
2017-05-06 11:53:46 +1000

    Improve the http server to use stream api in fs

--- httpServer.js
+++ httpServer.js
@@ -3,8 +3,8 @@ var fs = require('fs');
 
 http.createServer((req, res) => {
 
-    res.writeHead(200, { 'Content-Type': 'text/html'});
-    var fileContent = fs.readFileSync(__dirname + '/index.html', 'utf8');
-    res.end(fileContent);
+    res.writeHead(200, { 'Content-Type': 'video/mp4'});
+    var fileContent = fs.createReadStream(__dirname + '/movie.mp4');
+    fileContent.pipe(res);
 
 }).listen(7777, '127.0.0.1');
