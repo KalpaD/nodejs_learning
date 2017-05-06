ffe976efca1723e8af552f966f8fb3fafeca2832
Kalpa Senanayake <senanayake.kalpa@gmail.com>
2017-05-06 12:03:23 +1000

    Demo responding JSON from web server

--- httpServer.js
+++ httpServer.js
@@ -3,8 +3,15 @@ var fs = require('fs');
 
 http.createServer((req, res) => {
 
-    res.writeHead(200, { 'Content-Type': 'video/mp4'});
+    /*res.writeHead(200, { 'Content-Type': 'video/mp4'});
     var fileContent = fs.createReadStream(__dirname + '/movie.mp4');
-    fileContent.pipe(res);
+    fileContent.pipe(res);*/
+
+    res.writeHead(200, { 'Content-Type': 'application/json'});
+    var object = {
+        name: 'Kalpa',
+        age: '30'
+    }
+    res.end(JSON.stringify(object));
 
 }).listen(7777, '127.0.0.1');
